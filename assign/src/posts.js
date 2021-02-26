6a8a284cc3fc52e5b5fe62180e3be575342ebfa3
karthiik433 <kartheek12345kumar@gmail.com>
2021-02-26 10:15:30 +0530

    update2

--- assign/src/posts.js
+++ assign/src/posts.js
@@ -34,8 +34,8 @@ function Post(props){
         </>
         :
         <>
-        <thead style={{border:"2px solid black"}}>
-            <tr style={{border:"2px solid black"}}>
+        <thead>
+            <tr style={{border:"2px solid black",textAlign:"center"}}>
             <th style={{border:"2px solid black"}}>Serial Number:</th>
             <th style={{border:"2px solid black"}}>Title</th>
             <th style={{border:"2px solid black"}}>Author</th>
@@ -51,7 +51,7 @@ function Post(props){
                     setDat(JSON.stringify(item));
                     console.log(item);
                     setData(true)}}>
-                <td style={{border:"2px solid black"}}>{idx+1}</td>
+                <td style={{border:"2px solid black",textAlign:"center"}}>{idx+1}</td>
                 <td style={{border:"2px solid black"}}>{item.title}</td>
                 <td style={{border:"2px solid black"}}>{item.author}</td>
                 <td style={{border:"2px solid black"}}>{item.created_at}</td>
